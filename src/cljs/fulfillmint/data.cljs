593a88dc9a15c737f97b22966648d024c9d735b1
Daniel Leong <me@dhleong.net>
2018-09-03 12:05:25 -0400

    Begin spec'ing out what events/subs `data` should provide

--- src/cljs/fulfillmint/data.cljs
+++ src/cljs/fulfillmint/data.cljs
@@ -9,3 +9,35 @@
 ;; but that could change. The data ns is responsible for creating
 ;; re-frame subscriptions and events that act as abstractions on top
 ;; of the underlying storage format
+
+;; Subscriptions:
+;;
+;; [:products]
+;; all registered products, as {:id,:name}
+;;
+;; [:product <id>]
+;; details of a specific product by its id
+;;
+;; [:variants <product-id>]
+;; all "variants" of a given product, each as {:id,:name,:parts}
+;; where :parts is a mapping of <part-id> -> quantity.
+;;
+;; [:parts]
+;; all registered parts, each as {:id,:name,:quantity,:unit,:supplier}
+;;
+;; [:orders]
+;; all known orders, each as {:id,:buyer-name,:service,:service-id,:link,:variants,:done?}
+;;
+;; [:pending-orders]
+;; newly-imported orders from a service, each as {:id,:buyer-name,:service,:service-id,:link}
+
+
+;; Events:
+;;
+;; [:create-part {:id,:name,:quantity,:unit,:supplier}]
+;;
+;; [:create-product {:name,:service,:service-id}]
+;;
+;; [:create-variant <product-id> {:parts}]
+;;
+;; [:create-order ]
